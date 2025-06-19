06280dc59f0a88c2c1c2091de16e7d1b03093db5
Kev O'C <@kevoc>
2025-06-19 13:16:28 +0200

    Adding logging

--- src/tfi_gtfs/__init__.py
+++ src/tfi_gtfs/__init__.py
@@ -3,3 +3,8 @@ from . import settings
 from . import web_server
 
 from .gtfs import StaticAssets
+
+# TODO: build this into the application properly when
+#       the command line launcher is built.
+from .logger import log_to_stderr
+log_to_stderr(debug=False, verbose=False)
