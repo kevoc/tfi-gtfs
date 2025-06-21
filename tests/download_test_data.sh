9520af2e6210f98e864f66682a8520f6fd907f55
Kev O'C <@kevoc>
2025-06-21 18:06:55 +0200

    Updates to ignore files in tests/ folder

--- tests/download_test_data.sh
+++ tests/download_test_data.sh
@@ -4,5 +4,11 @@
 # your working directory should be the root of the git repo, not this folder.
 source settings.env
 
+rm tests/GTFS.zip tests/realtime_data.bin tests/asset_timestamp.txt
+
 curl -o tests/GTFS.zip "$GTFS_STATIC_URL"
-curl -o tests/live_data.bin -H "X-API-KEY: $API_KEY" "$GTFS_LIVE_URL"
+curl -o tests/realtime_data.bin -H "X-API-KEY: $API_KEY" "$GTFS_LIVE_URL"
+
+# the datetime.now() function will return this timestamp when the
+# server is testing with cached assets.
+date "+%s" > tests/asset_timestamp.txt
